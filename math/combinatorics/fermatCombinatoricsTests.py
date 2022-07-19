ccc714adc1474cb580c1e2def19e42fdf2cf6ede
alxwen711 <alxwen711@gmail.com>
2022-07-19 12:29:47 -0700

    fermatCombinatorics basic testfile

--- math/combinatorics/fermatCombinatoricsTests.py
+++ math/combinatorics/fermatCombinatoricsTests.py
@@ -1,10 +1,17 @@
-import fermatCombinatorics
+from fermatCombinatorics import fermat_array, fermat_comb
 import unittest
 
 class fermat_array_test(unittest.TestCase):
 
-    def dummy(self):
+    def test_dummy(self):
         self.assertEqual(1,1)
+
+    def test_basic(self):
+        r = 100000007
+        facts,invs = fermat_array(100,100,r)
+        n = 10
+        k = 0
+        self.assertEqual(fermat_comb(n,k,r,facts,invs),1)
 
 
 
