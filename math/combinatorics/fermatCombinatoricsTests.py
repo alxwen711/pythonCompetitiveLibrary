add6a32537a03cf13542085dd4267aa7548cde28
alxwen711 <alxwen711@gmail.com>
2022-07-19 12:21:51 -0700

    reverting previous folder structure change, keep test files with algorithmic files

---
+++ math/combinatorics/fermatCombinatoricsTests.py
@@ -0,0 +1,22 @@
+import fermatCombinatorics
+import unittest
+
+class fermat_array_test(unittest.TestCase):
+
+    def dummy(self):
+        self.assertEqual(1,1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
