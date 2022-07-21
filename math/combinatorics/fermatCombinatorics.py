e9788a49d4903a387cc5c3cc59f920e5658db0d3
alxwen711 <alxwen711@gmail.com>
2022-07-20 20:19:54 -0700

    basic documentation (comments) for fermatCombinatorics

--- math/combinatorics/fermatCombinatorics.py
+++ math/combinatorics/fermatCombinatorics.py
@@ -1,14 +1,25 @@
-#WIP, usable but documentation still needed
 """
-n choose k mod r
+Author: alxwen711 (Alex Wen)
+Last updated: 7/20/22
 
-describe each function in documentation here
-fermat_array -> create array for solving up to given n choose k
-fermat_comb -> for using fermat array
+Fermat algorithms for quickly calculating n choose k mod r
+Note: fermat_array does not work if r < 2, in this case use
+the single case function fermat_calc.
+
+fermat_array(n,k,r)
+returns two arrays facts and invs to be used with fermat_comb
+parameters inserted are the maximum values, ie. for calculating
+100 choose 50 mod 727 using this function, parameters should be
+fermat_array(100,50,727)
+
+
+fermat_comb(n,k,r,facts,invs)
+returns n choose k mod r
+must use fermat_array to get facts and invs
 
 """
 def fermat_array(n: int, k: int, r: int) -> list[list[int],list[int]]:
-    #factorials[n]*inverses[k]*inverses[n-k] % r for usage
+    #O(n log n) time
     if k >= r or r < 3: return None, None 
     arLen = n+k+3 #array length
     factorials = [0]*arLen
@@ -21,12 +32,16 @@ def fermat_array(n: int, k: int, r: int) -> list[list[int],list[int]]:
 
 
 def fermat_comb(n: int, k: int, r: int, facts: list[int], invs: list[int]) -> int:
-    if k > n or n < 0 or k < 0 or r < 0 or facts == None or invs == None: return -1 
+    #O(1) lookup time
+    if facts == None or invs == None or r == 0: return -1 #array or div 0 error
+    #trivial cases
+    if k > n or r == 1: return 0 
+    if n == 0 or k == 0: return 1
     return facts[n]*invs[k]*invs[n-k] % r
 
 
 if __name__ == "__main__":
-    #include tests here
+    #include example use here
     r = 100000007
     facts,invs = fermat_array(100,100,r)
     n = 10
