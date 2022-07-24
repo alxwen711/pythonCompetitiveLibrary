d312501814e377de2cb5e0c9a29e98aab01a9a2c
alxwen711 <alxwen711@gmail.com>
2022-07-23 20:56:21 -0700

    added template file

---
+++ implementation/template.py
@@ -0,0 +1,9 @@
+import sys
+
+#input/output functions
+readint = lambda: int(sys.stdin.readline())
+readints = lambda: map(int,sys.stdin.readline().split())
+readar = lambda: list(map(int,sys.stdin.readline().split()))
+print = sys.stdout.write
+flush = lambda: sys.stdout.flush()
+
