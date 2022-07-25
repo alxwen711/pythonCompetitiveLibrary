c089846777deeb4c250a6fbbbd4011285048e344
alxwen711 <alxwen711@gmail.com>
2022-07-25 11:20:31 -0700

    hashmaps template file to complete later

---
+++ data structures/hashmaps.py
@@ -0,0 +1,34 @@
+#WIP
+"""
+Author: alxwen711 (real name, optional)
+Last updated: [date]
+
+frequency map/hash table file
+
+given array, return frequency of each value in array/dict
+with dict exact positions can be located
+letter variations can be included here as well
+
+func_a(x,y,z)
+[explain func_a and its parameters]
+func_b(x,y)
+[explain func_b and its parameters]
+Other useful things to note for documenting:
+- Edge cases that the algorithm may not work for
+- Runtime (O(1),O(log n),O(n),O(n^2),etc.)
+"""
+
+
+
+
+
+def func_a(x,y,z) -> str:
+    return "hello"
+
+
+def func_b(x,y) -> str:
+    return "world"
+
+if __name__ == "__main__":
+    #[include an example use here]
+    print(func_a(1,1,1),func_b(1,1))
