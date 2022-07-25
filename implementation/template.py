3d2be67cff0bde77f33737088b163d79ed575a6d
alxwen711 <alxwen711@gmail.com>
2022-07-24 21:56:30 -0700

    removed print lambda from base template, see comments for more details

--- implementation/template.py
+++ implementation/template.py
@@ -1,9 +1,8 @@
 import sys
 
-#input/output functions
+#input functions
 readint = lambda: int(sys.stdin.readline())
 readints = lambda: map(int,sys.stdin.readline().split())
 readar = lambda: list(map(int,sys.stdin.readline().split()))
-print = sys.stdout.write
 flush = lambda: sys.stdout.flush()
 
