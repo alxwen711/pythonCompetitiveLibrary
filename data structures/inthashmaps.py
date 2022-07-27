eaed381be34d106233a0f2c1a0a88c6b08a5fdec
alxwen711 <alxwen711@gmail.com>
2022-07-27 11:12:40 -0700

    inthashmaps doc fix

--- data structures/inthashmaps.py
+++ data structures/inthashmaps.py
@@ -13,6 +13,8 @@ Returns a frequency array h where h[x-1] contains the number of
 times value x shows up in ar. limit is supposed to be the maximum
 value in ar and controls the size of h. If ar is None then an empty
 array is returned.
+Warning: freq_ar will exceed memory limits of most contests if limit
+is too high. On Codeforces this will happen if limit = 10^9.
 
 freq_dict(ar,pos)
 Returns a dictionary d where d[x] contains either:
@@ -25,9 +27,9 @@ from random import randint #for example use
 
 def freq_ar(ar: list[int], limit: int) -> list[int]:
     if ar == None: return []
-    h = [0]*limit #change to limit+1 if you want h[1] = freq of 1
+    h = [0]*limit #change to limit+1 for h[1] = freq of 1
     for i in range(len(ar)):
-        h[ar[i]-1] += 1 #change to h[ar[i]] if you want h[1] = freq of 1
+        h[ar[i]-1] += 1 #change to h[ar[i]] for h[1] = freq of 1
     return h
 
 def freq_dict(ar: list[int], pos: bool) -> dict:
