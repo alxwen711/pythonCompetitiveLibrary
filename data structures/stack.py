146c64db26e0c9d925a75fbd88080c22280efa8a
alxwen711 <alxwen711@gmail.com>
2022-08-02 11:34:10 -0700

    stack file completed

--- data structures/stack.py
+++ data structures/stack.py
@@ -1,11 +1,20 @@
 #WIP
 """
 Author: alxwen711 (Alex Wen)
+Last updated: August 2nd, 2022
 
-Basic stack class template
+Template for the stack data structure. The stack is made up of a 1D
+mutuable array. All stack methods are optimized to run as fast as
+possible.
 
+push(x)
+adds element x to the stack.
 
+pop()
+returns and removes the top element in the stack.
 
+top()
+returns the top element in the stack, but does not remove it.
 """
 
 
@@ -19,18 +28,20 @@ class stack:
     def push(self,x) -> None:
         self.s.append(x)
 
+    def pop(self):
+        x = self.s.pop()
+        return x
+
     def top(self):
         return self.s[-1]
-
-    def pop(self):
-        x = self.s.pop(-1)
-        return x
 
 
 
 if __name__ == "__main__":
-    x = stack()
+    x = stack() #create new stack
     x.push(1)
     x.push(2)
     x.push(3)
-    print(x.pop())
+    #stack is now [1,2,3]
+    print(x.pop()) #should be 3
+    print(x.top()) #should be 2
