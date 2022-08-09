112fe75ab98691fb69902855a508940cad9e6878
alxwen711 <alxwen711@gmail.com>
2022-08-08 17:40:49 -0700

    template test files to be edited, all files with completed testfiles are deleted in test folder

---
+++ tests/algorithms/search algorithms/binarySearchTests.py
@@ -0,0 +1,17 @@
+import sys
+import unittest
+sys.path.append("../") #insert path to directory here
+
+class algorithm_test(unittest.TestCase):
+
+    def test_a(self): #insert test here
+        print("test a")
+        self.assertEqual(1,1)
+
+    def test_b(self): #insert test here
+        print("test b")
+        self.assertEqual(1,1)
+
+
+if __name__ == "__main__":
+    unittest.main()
