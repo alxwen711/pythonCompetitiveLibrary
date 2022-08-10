cd9cfa2ce28ed3cbd29be7d522a6d2b8bf95da1e
alxwen711 <alxwen711@gmail.com>
2022-08-10 16:09:38 -0700

    tests for freq_ar in inthashmaps.py

--- data structures/inthashmapsTests.py
+++ data structures/inthashmapsTests.py
@@ -1,16 +1,32 @@
 import sys
 import unittest
-sys.path.append("../") #insert path to directory here
+from inthashmaps import *
+from random import randint
 
-class algorithm_test(unittest.TestCase):
+class ar_test(unittest.TestCase):
 
-    def test_a(self): #insert test here
-        print("test a")
-        self.assertEqual(1,1)
+    def test_base(self): #100 random arrays, 20 values each from 1 to 10
+        for i in range(100):
+            ar = list()
+            for j in range(20):
+                ar.append(randint(1,10))
+            br = freq_ar(ar,10)
+            for k in range(10):
+                assert br[k] == ar.count(k+1)
+                
+    def test_speed(self): #array with 448 1's, 448 2's... 448 448's
+        #this test takes under 0.1s, ~200000 operations
+        ar = list()
+        for i in range(448):
+            for j in range(448):
+                ar.append(i+1)
+        br = freq_ar(ar,448)
+        for k in range(448):
+            assert br[k] == 448
 
-    def test_b(self): #insert test here
-        print("test b")
-        self.assertEqual(1,1)
+#class dict_test(unittest.TestCase): #assumes freq_ar is working
+
+    
 
 
 if __name__ == "__main__":
