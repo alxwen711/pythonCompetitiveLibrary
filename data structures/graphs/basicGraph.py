0ed05690ec765111577e010823c773bfa17ad679
alxwen711 <alxwen711@gmail.com>
2022-08-14 20:14:21 -0700

    basicGraph.py for general graph creation, renamed trees folder to more broad category graphs

---
+++ data structures/graphs/basicGraph.py
@@ -0,0 +1,42 @@
+"""
+Author: alxwen711 (Alex Wen)
+Last updated: August 14th, 2022
+
+Basic graph creating structure, specifically for input format found
+in most programming competitions.
+
+The first value is the number of nodes in the graph
+Then follows a series of lines with values u and v
+representing an edge between two nodes.
+The edges assume the nodes are being referred to as 1 to n (no node #0)
+"""
+
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.connected = list()
+
+    def add(self,x):
+        self.connected.append(x)
+
+def nodeList(n: int) -> list[Node]:
+    ar = list()
+    ar.append(None)
+    for i in range(1,n+1):
+        ar.append(Node(i))
+    return ar
+
+def edge(a: Node, b: Node):
+    a.add(b)
+    b.add(a)
+
+
+
+if __name__ == "__main__":
+    #example use
+    graph = nodeList(5)
+    edge(graph[1],graph[3]) #edge between node 1 and 3
+    edge(graph[1],graph[2]) #1 and 2
+    edge(graph[4],graph[5]) #4 and 5
+    edge(graph[2],graph[4]) #2 and 4
+    
