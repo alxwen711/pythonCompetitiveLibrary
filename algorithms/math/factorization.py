dada89b80681b154cbc938ab5c3881862d57547d
alxwen711 <alxwen711@gmail.com>
2022-08-23 21:37:05 -0700

    factorize function using Pollard's rho algorithm, still need to implement varying parameters

--- algorithms/math/factorization.py
+++ algorithms/math/factorization.py
@@ -13,13 +13,13 @@ factorize returns factors in 2d array, each array is [factor, freq]
 """
 from math import gcd
 
-def fact_func(x: int, r: int) -> int:
+def fact_func(x: int, r: int) -> int: #may need to use other c's than 1
     return (x*x+1) % r
 
 
 def find_factor(n: int) -> int:
     x = 2
-    y = 2
+    y = x
     #i = 0
     a = 1
     while True: #unsure how many iterations are needed here
@@ -31,11 +31,32 @@ def find_factor(n: int) -> int:
             break
         if x == y:
             break
+        """
+        if a == n: #invalid case, try with new parameters
+        after enough attempts then prime can be called
+        """
     #print(i)
     return a
 
 def factorize(n: int) -> list[list[int]]:
-    return None
+    d = {}
+    while n != 1:
+        prev = find_factor(n)
+        x = 0
+        while True:
+            print(n,prev)
+            x = find_factor(prev)
+            if x == prev:
+                break
+            else: prev = x
+        if d.get(x) == None: d[x] = 0
+        d[x] += 1
+        n = n // x
+    ar = list()
+    k = list(d.keys())
+    for i in range(len(k)):
+        ar.append([k[i],d[k[i]]])
+    return ar
 
 
 
@@ -46,4 +67,6 @@ def factorize(n: int) -> list[list[int]]:
 
 
 if __name__ == "__main__":
-    print(find_factor(1497631652873))
+    #print(find_factor(1497631652873))
+    print(find_factor(16))
+    print(factorize(720))
