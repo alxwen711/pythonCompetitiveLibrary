3714c5cfde198786edefb9deabdf88957fd8703b
alxwen711 <alxwen711@gmail.com>
2022-08-25 15:19:35 -0700

    factorization.py refactoring

--- algorithms/math/factorization.py
+++ algorithms/math/factorization.py
@@ -13,52 +13,52 @@ these will be removed once the algorithm is fully functional.
 
 factorize returns factors in 2d array, each array is [factor, freq]
 """
-from math import gcd,sqrt,ceil
+from math import gcd,sqrt,ceil,inf
 
 def fact_func(x: int, r: int) -> int: #may need to use other c's than 1
     return (x*x+1) % r
 
 
 def find_factor(n: int) -> int:
-    x = 2
-    y = x
-    #i = 0
-    a = 1
-    while True: #unsure how many iterations are needed here
+    x,y,a = 2,2,1
+
+    while a != 1: #unsure how many iterations are needed here
         #i += 1
         x = fact_func(x,n)
         y = fact_func(fact_func(y,n),n)
         a = gcd(n,abs(x-y))
-        if a != 1:
-            break
         if x == y:
             break
+
     return a
 
-def div(n: int, ar: list) -> list[int]:
+def div(n: int, ar: list, limit = inf) -> list[int]:
+
     if n == 1: return ar
     if n % 2 == 0:
         ar.append(2)
-        return div(n//2,ar)
-    for i in range(3,ceil(sqrt(n+1)),2):
+        return div(n//2,ar,limit)
+
+    for i in range(3,min(limit,ceil(sqrt(n+1))),2): #3,5,7,...,sqrt(n)
         if n % i == 0:
             ar.append(i)
-            return div(n//i,ar)
+            return div(n//i,ar,limit)
+    #n is prime
     ar.append(n)
     return ar
 
-def trial_div(n: int) -> list[int]:
+def trial_div(n: int, limit = inf) -> list[int]:
     if n == 2 or n == 3: return [n]
-    else: return div(n,list())
+    else: return div(n,list(),limit)
 
 def factorize(n: int) -> list[list[int]]:
+    #break n into smaller factors
     d = {}
     while n != 1:
         x = find_factor(n)
         if d.get(x) == None: d[x] = 0
         d[x] += 1
         n = n // x
-    ar = list()
     k = list(d.keys())
     #print(d)
     #check each factor using trial div, not all may be prime
@@ -73,15 +73,13 @@ def factorize(n: int) -> list[list[int]]:
                 if d.get(c[m]) == None: d[c[m]] = 0
                 d[c[m]] += b
             d[a] = 0
+    #translate from dict to list[int]
     k = list(d.keys())
+    ans = list()
     for j in range(len(k)):
         if d[k[j]] != 0:
-            ar.append([k[j],d[k[j]]])
-    
-    return ar
-
-
-
+            ans.append([k[j],d[k[j]]])
+    return ans
 
 
 
@@ -89,6 +87,7 @@ def factorize(n: int) -> list[list[int]]:
 
 
 if __name__ == "__main__":
+    #wip testing, formal testcases are wip
     #print(find_factor(1497631652873))
     #print(find_factor(16))
     print(factorize(720))
