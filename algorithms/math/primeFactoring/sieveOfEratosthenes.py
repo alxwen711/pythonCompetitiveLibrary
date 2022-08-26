413e4f0456d76e5216aaf4594ca74d5cf7ed418b
alxwen711 <alxwen711@gmail.com>
2022-08-26 15:34:18 -0700

    sieve doc edits

--- algorithms/math/primeFactoring/sieveOfEratosthenes.py
+++ algorithms/math/primeFactoring/sieveOfEratosthenes.py
@@ -1,18 +1,17 @@
-#WIP
 """
 Author: Alex Wen
-Last updated: August 22nd, 2022
+Last updated: August 26th, 2022
 
 Sieve of Eratosthenes implementation for finding all primes up to n.
-Runtime is O(n log log n), which is close enough to O(n) for most uses.
+Runtime is O(n log log n), which is close enough to O(n) for practical use.
 
 sieve(n)
 Returns an array with n+1 elements. If A[x] == True, x is prime,
-otherwise x is composite.
+else x is composite.
 
 fsieve(n)
 Returns an array with n+1 elements. If A[x] == 1, x is prime,
-otherwise x's smallest prime factor is stored (useful for prime factorization).
+else x's smallest prime factor is stored (useful for prime factorization).
 """
 from math import sqrt, floor
 
@@ -29,13 +28,11 @@ def sieve(n: int) -> list[bool]:
 def fsieve(n: int) -> list[int]: 
     ar = [1]*max(2,(n+1))
     ar[0] = 0
-    ar[1] = 0
     for i in range(2,floor(sqrt(n))+1):
         if ar[i] == 1: #i is prime
             for j in range(i,n//i+1):
                 if ar[i*j] == 1: ar[i*j] = i
     return ar
-
 
 if __name__ == "__main__":
     print(fsieve(20))
