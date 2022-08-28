17d4da09870d401ec07744e782b83c74bcfc45af
alxwen711 <alxwen711@gmail.com>
2022-08-28 10:14:09 -0700

    sparseTableTests.py refactoring

--- data structures/sparseTableTests.py
+++ data structures/sparseTableTests.py
@@ -1,17 +1,15 @@
-import sys
 import unittest
-from sparseTable import * #change to filename
+from sparseTable import * 
 from random import randint
 
 class sparse_test(unittest.TestCase):
 
-    def test_normal(self):
+    def test_normal(self): #test every range possible in small array
         x = [7,8,7,0,9,8,6,4,3,1,9,2,0,0,89,2]
-        a = create_sparse(x)
-        #print(a), contains sparse table
+        a = create_sparse(x) #create table
+        assert a != None
         for i in range(len(x)):
             for j in range(i,len(x)):
-                #print(i,j,query(i,j,a)) for validation
                 assert query(i,j,a) == min(x[i:j+1])
     
     def test_speed(self):
@@ -20,17 +18,17 @@ class sparse_test(unittest.TestCase):
         ar = [0]*n
         for i in range(n):
             ar[i] = i
-        t = create_sparse(ar) #table
-        for j in range(n):
+        t = create_sparse(ar) #create table
+        for j in range(n): #100000 random ranges, each with O(1) runtime
             a,b = randint(0,n-1),randint(0,n-1)
             assert query(min(a,b),max(a,b),t) == min(a,b)
 
-    def test_single(self):
+    def test_single(self): #test single value ranges
         n = 1000
         ar = [0]*n
         for i in range(n):
             ar[i] = randint(0,1000000000)
-        t = create_sparse(ar) #table
+        t = create_sparse(ar) #create table
         for j in range(n):
             assert query(j,j,t) == ar[j]
 
