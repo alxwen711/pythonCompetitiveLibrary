203db4b276816614abc9c786393d3944d0b261af
alxwen711 <alxwen711@gmail.com>
2022-08-29 20:17:16 -0700

    stddev func

--- algorithms/math/statistics.py
+++ algorithms/math/statistics.py
@@ -4,16 +4,25 @@ Author: alxwen711 (Alex Wen)
 Last update: August 25th, 2022
 
 Statistics functions
+
+avg(ar)
+
+median(ar)
+
+mode(ar)
+
+std_dev(ar)
+
 """
 
 
-
-
 def avg(ar: list):
+    if len(ar) == 0: return None
     return sum(ar)/len(ar)
 
 
 def median(ar: list):
+    if len(ar) == 0: return None
     br = ar.copy()
     br.sort()
     x = len(ar)
@@ -28,6 +37,13 @@ def mode(ar: list):
     """
     use freq_dict in some way here? return as list for mult modes
     """
+    if len(ar) == 0: return None
+    d = {}
+    for i in range(len(ar)):
+        x = ar[i]
+        if d.get(x) == None: d[x] = 0
+        d[x] += 1
+    #use keys here
     return 1
 
 
@@ -39,10 +55,23 @@ def std_dev(ar: list):
     sum each y found, divide by num of elements
     sqrt for std dev
     """
+    mean,n = avg(ar),len(ar)
+    diff = 0
+    for i in range(n):
+        diff += (mean-ar[i])**2
+    return (diff / n) ** 0.5
 
 
 
 
-
+from random import randint
 if __name__ == "__main__":
-    #examples
+    ar = list()
+    for i in range(10):
+        ar.append(randint(0,10))
+    print("array:",ar)
+    print("mean:",avg(ar))
+    print("median:",median(ar))
+    print("mode:",mode(ar))
+    print("stddev:",std_dev(ar))
+    
