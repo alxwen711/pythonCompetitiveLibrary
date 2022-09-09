6ad7e1234eb944842103fc402cee51db218faf60
alxwen711 <alxwen711@gmail.com>
2022-09-08 21:03:24 -0700

    secureHash full algorithm

--- data structures/secureHash.py
+++ data structures/secureHash.py
@@ -8,23 +8,38 @@ This is designed to be a dictionary/hashmap structure
 that is much harder to exploit. For now I'll call it
 the Randomized Anti Codeforces Hashmap (RACH).
 
-Note that I'm designing this to be as convoluted as possible,
-so the normal guidelines of "clear and understandable" code
-are not in effect here.
+Currently supports integer keys only. Float keys may be
+added if I find python's normal dict to be exploitable
+there as well.
 
-Good luck hacking this.
+Good luck hacking this. Only drawback is that using
+this is slightly inconvinient, but it's easily better
+than losing 100 ELO due to being hacked. Yes I may
+still be salty over one specific incident.
 """
 
-from secrets import randbelow as r 
-_z = 2**64-1
+from secrets import randbelow
+from random import shuffle
+_MAX = 2**64-1
 class hashmap:
     def __init__(self):
-        self.f,self.q,self.l = r(_z),list(),list()
-        self.d,self.c = list(),r(420)
-
-
+        self.f,self.q,self.l = randbelow(420),list(),list()
+        self.d,self.c = list(),randbelow(_MAX)
+        for potato in range(100):
+            self.d.append(randbelow(_MAX))
+            self.l.append({})
+            self.q.append(potato)
+        shuffle(self.q)
+        
+    def decode(self, x: int) -> list[dict,int]:
+        x = x ^ self.c
+        xx = (x+self.f) % 100
+        hm = self.l[self.q[xx]]
+        x = x ^ self.d[xx]
+        return hm,x
 
 
 if __name__ == "__main__":
     print("lol you ain't breaking this")
-
+    d = hashmap()
+    print(d.decode(100))
