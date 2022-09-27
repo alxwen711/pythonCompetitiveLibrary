e62c906db8050091b024fd795bee2847dd76a21d
alxwen711 <alxwen711@gmail.com>
2022-09-27 11:41:13 -0700

    diophantine.py updated docs, removed commented old version

--- algorithms/math/diophantine.py
+++ algorithms/math/diophantine.py
@@ -1,19 +1,17 @@
 """
 Author: alxwen711 (Alex Wen)
-Last updated: September 13th, 2022
+Last updated: September 27th, 2022
 
-Diophantine equation solver, ie. equations in the form ax + by = c
+Diophantine equation solver
+ie. algorithm that tries to find integer (x,y)
+solutions for equations in the form ax + by = c
+
 Algorithm is modelled off of https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Example
 
 dio(a,b,c)
-
 For the equation ax+by = c, a solution in the form (x,y) is returned as
 4 values (s,t,u,v), where all possible solutions are in the form (s+nu,t-nv)
 n is an integer.
-
-returns two values containing the integer solution to ax+by = c
-first value returned is a possible x value that gives an integer solution
-second value returned is the positive increment for x.
 
 If (-1,-1,-1,-1) is returned, then there is no solution.
 
@@ -28,16 +26,16 @@ def dio(a: int, b: int, c: int):
     #trivial a = b case
     if a == b: 
         if c % a != 0:
-            #return -1,-1
             return -1,-1,-1,-1
         else:
-            #return 0,1
             return 0,c//b,1,1
         
-    #use extended euclidian algorithm
+    #extended euclidian algorithm
     g = list() #gcd
     s = list() #a coeff
     t = list() #b coeff
+
+    #initialization
     if a > b:
         g.append(a);g.append(b)
         s.append(1);t.append(0)
@@ -46,6 +44,7 @@ def dio(a: int, b: int, c: int):
         g.append(b);g.append(a)
         s.append(0);t.append(1)
         s.append(1);t.append(0)
+        
     while True:
         q = g[-2] // g[-1]
         r = g[-2]-(g[-1]*q)
@@ -53,16 +52,16 @@ def dio(a: int, b: int, c: int):
         g.append(r)
         s.append(s[-2]-(s[-1]*q))
         t.append(t[-2]-(t[-1]*q))
+        
+    #check if solution exists
     if c % g[-1] != 0: return -1,-1,-1,-1
 
-    #solution exists s[-1],t[-1]
-    ansa,ansb = s[-1],t[-1]
+    ansa,ansb = s[-1],t[-1] #one possible solution
     ansa *= c//g[-1]
     ansb *= c//g[-1]
     ac = b//g[-1]
     bc = a//g[-1]
 
-    #return ansa,ac
     return ansa,ansb,ac,bc
 
 
