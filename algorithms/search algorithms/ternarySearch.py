d22f039a63b15b9dedae5937e002dac3e1c19457
alxwen711 <alxwen711@gmail.com>
2022-11-04 14:17:30 -0700

    ternarySearch docs

--- algorithms/search algorithms/ternarySearch.py
+++ algorithms/search algorithms/ternarySearch.py
@@ -1,31 +1,44 @@
 """
 Author: alxwen711 (Alex Wen)
-Last updated: October 4th, 2022
+Last updated: November 4th, 2022
 
-Ternary searching implementation, docs to be updated soon
+Ternary searching implementation, below the code has
+been fitted for example usage as follows:
+
+Given a "mountain" array (given n elements, for some
+index i, A[:i] is strictly non-decreasing and A[i:]
+is strictly non-increasing), find the largest value.
+Ternary search can be used to find the largest value.
+Choose two indicies a,b where 0 < a < b < n. If A[a] > A[b]
+the max val has to be in A[:b]. if A[a] < A[b] the max val
+has to be in A[a:]. If A[a] = A[b] max val is in A[a:b]. By
+choosing a and b such that the remaining search range is split
+into thirds, the program will effectively run in O(log n) time.
 
 
 tfunc(x,ar)
-Boolean function to determine if the low or high endpoint of the binary
-search range needs to be adjusted. ar is an optional value that can be
-passed depending on the problem.
+Ternary function that will return 0, 1, or 2 depending on the input.
+In practical use this should be adjusted as necessary for the problem.
 
 tsearch(low,high,ar)
-Binary searching framework. low and high are the range endpoints to
-binary search on. ar is an optional value that can be passed depending
-on the problem.
+Ternary searching framework with low, high as the starting range limits.
+ar is an optional value to use depending on the problem being solved.
+The searching method is currently set to search the bottom 2/3rds of the
+range if tfunc = 0, the top 2/3rds of the range if tfunc = 1, and the middle
+1/3 if tfunc = 2. If the range is small enough (in this case, 3 or fewer values
+to search), then the algorithm will brute force the solution on the remaining
+values. Like tfunc, tsearch will be adjusted as needed based on the problem.
+
 
 IMPORTANT NOTES:
-The functions are default set to adjust the high endpoint if bfunc returns
-True and to adjust the low endpoint if bfunc returns False.
+The last line in tsearch is meant to brute force the answer over a small range
+of remaining possible values and is dependent on problem being solved.
 
-The second last line in bsearch may need to be adjusted to test the low endpoint
-depending on the problem.
-
-If the range being searched is not solely integers, replace bsearch's first two
+If the range being searched is not solely integers, replace tsearch's first three
 lines with the following:
 while high - low > x: (x = maximum error allowed)
-    mid = (low+high)/2
+    left = (high-low)/3+low
+    right = (2*(high-low))/3+low
 """
 
 def tfunc(left, right, ar = None):
@@ -41,14 +54,16 @@ def tsearch(low, high, ar = None):
         left = (high-low)//3+low
         right = (2*(high-low))//3+low
         x = tfunc(left,right,ar)
-        if x == 0: low = left
-        elif x == 1: high = right
-        else:
+        if x == 0: low = left #left case
+        elif x == 1: high = right #right case
+        else: #center case
             low = left
             high = right
-            
+
     #3 or fewer values in range, code edge case here
     return max(ar[low],ar[high],ar[(low+high)//2])
+
+
 if __name__ == "__main__":
     #example use, guessing val from 1 to 1000000
     mountain_ar = [0,2,7,8,9,11,7,6,5,3,1]
