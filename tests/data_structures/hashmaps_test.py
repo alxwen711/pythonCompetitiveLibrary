3f835aa8db012a05091ee6e7530d151576bdb08e
alxwen711 <alxwen711@gmail.com>
2025-12-17 09:11:29 -0800

    reorganize folder structure, setup first pytest

---
+++ tests/data_structures/hashmaps_test.py
@@ -0,0 +1,55 @@
+import sys
+import pytest
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+from src.data_structures.hashmaps import *
+from random import randint
+
+def test_base(): #100 random arrays, 20 values each from 1 to 10
+    for i in range(100):
+        ar = list()
+        for j in range(20):
+            ar.append(randint(1,10))
+        br = freq_ar(ar,10)
+        for k in range(10):
+            assert br[k] == ar.count(k+1)
+                
+def test_speed(): #array with 448 1's, 448 2's... 448 448's
+    #this test takes under 0.1s, ~200000 operations
+    ar = list()
+    for i in range(448):
+        for j in range(448):
+            ar.append(i+1)
+    br = freq_ar(ar,448)
+    for k in range(448):
+        assert br[k] == 448
+
+def test_freq(): #test pos = False
+    ar = list()
+    for i in range(100000):
+        ar.append(randint(1,100000))
+    d = freq_dict(ar)
+    test = freq_ar(ar,100000)
+    for j in range(100000):
+        x = test[j]
+        if x == 0:
+            assert d.get(j+1) == None
+        else:
+            assert d[j+1] == x 
+
+def test_pos(): #test pos = True
+    ar = list()
+    for i in range(100000):
+        ar.append(randint(1,100000))
+    d = freq_dict(ar,True)
+    correct = 0
+    for j in range(100000):
+        if d.get(j+1) != None:
+            for k in range(len(d[j+1])):
+                if ar[d[j+1][k]] == j+1: correct += 1
+    assert correct == 100000
+
+    
+
+    
+
