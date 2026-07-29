fed10694d965e8eb68e502ff589686239d1fb8b3
alxwen711 <alxwen711@gmail.com>
2026-07-28 21:06:01 -0700

    revise test templates to use pytest

---
+++ utility/algorithmTemplate_test.py
@@ -0,0 +1,10 @@
+import pytest
+
+from utility.algorithmTemplate import *
+
+
+def test_func_a(): #insert test here
+    assert func_a(1,"a",True) == "hello"
+
+def test_func_b(): #insert test here
+    assert func_b(1,2) == "world"
---
+++ utility/testcaseGenerator_test.py
@@ -0,0 +1,11 @@
+import pytest
+
+from utility.algorithmTemplate import *
+
+# TODO: implement actual generator tests here
+
+def test_func_a(): #insert test here
+    assert func_a(1,"a",True) == "hello"
+
+def test_func_b(): #insert test here
+    assert func_b(1,2) == "world"
